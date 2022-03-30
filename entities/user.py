7cad00da3cccc65d41d7b971bf222ddbaa5d2708
HorusLC <dkostadinov_11@abv.com>
2022-03-30 19:02:03 +0300

    User entity added

---
+++ entities/user.py
@@ -0,0 +1,16 @@
+class User:
+
+    def __init__(self, id_, f_name, l_name, username, password, salt, gender, email, date_reg, role):
+        self.role = role
+        self.date_reg = date_reg
+        self.email = email
+        self.gender = gender
+        self.salt = salt
+        self.password = password
+        self.username = username
+        self.l_name = l_name
+        self.f_name = f_name
+        self.id_ = id_
+
+    def __str__(self):
+        return self.f_name + self.l_name
