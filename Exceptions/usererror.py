8ef2cc3f4bb6839ed7d8294a3b666e5e7a3bbe82
HorusLC <39743315+HorusLC@users.noreply.github.com>
2022-04-06 00:23:12 +0300

    some custom exceptions for the users

---
+++ Exceptions/usererror.py
@@ -0,0 +1,13 @@
+class EmailNotFoundError(Exception):
+    # TODO add actual implementation
+    pass
+
+
+class UsernameNotFoundError(Exception):
+    # TODO add actual implementation
+    pass
+
+
+class IncorrectPasswordError(Exception):
+    # TODO add actual implementation
+    pass
