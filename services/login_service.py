380a48cb5d2471544f79485ff58e544f06bf386f
HorusLC <39743315+HorusLC@users.noreply.github.com>
2022-04-06 00:23:45 +0300

    login service with password checking

---
+++ services/login_service.py
@@ -0,0 +1,25 @@
+from Exceptions.usererror import UsernameNotFoundError, IncorrectPasswordError
+from dao.blogger_repository import BloggerRepository
+from entity.blogger import Blogger
+from services.phasher import PassHasher
+from datetime import datetime as dt
+from datetime import date
+
+
+class Login:
+    def __init__(self, hasher: PassHasher, blogger_repo: BloggerRepository):
+        self.hasher = hasher
+        self.blogger_repo = blogger_repo
+
+    def try_login(self, username, password):
+        self.blogger_repo.load()  # not sure if we should refresh here, or in the find_by_uname() repo function
+        user_found: Blogger = self.blogger_repo.find_by_username(username)
+        if user_found is None:
+            raise UsernameNotFoundError(f'No such username: {username} found!')
+        if not self.hasher.check_pass(user_found.password, user_found.salt, password):
+            raise IncorrectPasswordError("The provided username and password don't match!")
+        today = date.today()
+        user_found.last_login = today.strftime("%m/%d/%y")
+        self.blogger_repo.update(user_found)
+        self.blogger_repo.save()
+        return user_found
